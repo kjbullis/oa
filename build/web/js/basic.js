06a209db6b3904f909e628648b856a70141645b3
Kevin Bullis <kevin.bullis@gmail.com>
2017-01-16 09:22:57 -0600

    smooth preview working

--- build/web/js/basic.js
+++ build/web/js/basic.js
@@ -1,7 +1,37 @@
 //run a .each loop for all the tab elements, positioning them each += 15 degress (or whatever it is) more than the last one so they're arranged around a circle.
 $(document).ready(function () {
+	var originalSpotX,
+			originalSpotY;
+
+	distributeDots();
+
+	//create a way to check if an element exists
+	$.fn.exists = function () {
+		return this.length !== 0;
+	};
+
+	function centerDot() {
+		var gear = $("#gear-container"),
+				start = gear.offset(),
+				gWidth = gear.width(),
+				dWidth = gWidth * 0.4545454545,
+				x = start.left + gWidth / 2 - dWidth,
+				y = start.top + gWidth / 2 - dWidth;
+		$('.preview .dot').animate({
+			left: x + 'px',
+			top: y + 'px'
+		}, 250);
+	}
+
+	function putDotBack() {
+		$('.preview .dot').animate({
+			left: originalSpotX + 'px',
+			top: originalSpotY + 'px'
+		}, 250);
+	}
+
 	function distributeDots() {
-		var		gear = $("#gear-container"),
+		var gear = $("#gear-container"),
 				start = gear.offset(),
 				width = gear.width(),
 				dot = $('.dot'), //here "dot" is an array containing each element with the class dot
@@ -13,7 +43,7 @@ $(document).ready(function () {
 				startX = start.left + width / 2,
 				startY = start.top + width / 2;
 		dot.each(function () {
-//equation of a circle
+			//equation of a circle
 			//x = originX + radius * cos(angle in radians, or angle plus step in this case)
 			//y = originY + radius * sin(angle in radians, or angle plus step in this case)
 			var x = (startX + radius * Math.cos(angle)) - dotRadius,
@@ -26,36 +56,71 @@ $(document).ready(function () {
 		});
 	}
 
-	$(window).on('resize', function () {
-		distributeDots();
+//PREVIEW PRODUCTS WITH SWIPING ON PORTRAIT MODE
+	$(".swipe-here").on("swipeleft", function () {
+		putDotBack();
+		//check if there is a next anchor
+		if ($('.highlight').closest('a').next("a").exists()) {
+			//find the dot with the class "highlight", remove that class, find the next dot, add the class
+			$('.highlight').removeClass('highlight').closest('a').next().find('.shadow').addClass('highlight');
+			$('.preview').removeClass('preview').closest('a').next().addClass('preview').focus();
+			var here = $('.preview .dot').offset();
+				originalSpotX = here.left;
+				originalSpotY = here.top;
+			$('.preview .dot', function () {
+				centerDot();
+			});	
+		} else {
+			// if there is no next anchor, select the first anchor
+//			console.log("got to else statement -- no next a exists");
+			$('.highlight').removeClass('highlight');
+			$('.preview').removeClass('preview');
+			$('#dots-container a:first').addClass('preview').find('.shadow').addClass('highlight').focus();
+			var here = $('.preview .dot').offset();
+				originalSpotX = here.left;
+				originalSpotY = here.top;
+			$('.preview .dot', function () {
+				centerDot();
+			});	
+		}
 	});
 
-//	$("a").click(function () {
-//		$(this).find('img').css(
-//				'box-shadow', '0px 0px 0px black'
-//				);
-//	});
-	
-	
-	distributeDots();
+	$(".swipe-here").on("swiperight", function () {
+		putDotBack();
+		//check if there is a previous anchor
+		if ($('.highlight').closest('a').prev("a").exists()) {
+			//find the dot with the class "highlight", remove that class, find the next dot, add the class
+			$('.highlight').removeClass('highlight').closest('a').prev().find('.shadow').addClass('highlight');
+			$('.preview').removeClass('preview').closest('a').prev().addClass('preview').focus();
+			var here = $('.preview .dot').offset();
+				originalSpotX = here.left;
+				originalSpotY = here.top;
+			$('.preview .dot', function () {
+				centerDot();
+			});	
+		} else {
+			// if there is no previous anchor, select the last anchor
+//			console.log("got to else statement -- no next a exists");
+			$('.highlight').removeClass('highlight');
+			$('.preview').removeClass('preview');
+			$('#dots-container a:last').addClass('preview').find('.shadow').addClass('highlight').focus();
+			var here = $('.preview .dot').offset();
+				originalSpotX = here.left;
+				originalSpotY = here.top;
+			$('.preview .dot', function () {
+				centerDot();
+			});	
+		}
+	});
+
+	$(window).on('resize', function () {
+		distributeDots();
+		if ($(window).width >= $(window).height) {
+			$('.preview').removeClass('preview');
+		}
+	});
 
 
-//Build page with form functions 	
-
-//	function createDots() {
-//		dot.remove();
-//		for (var i = 0; i < +$('input:number').val(); i++) {
-//			$('<div/>', {
-//				'class': 'dot'
-//			}).appendTo(container);
-//		}
-//	};
-//	//	
-//	$("form").on("submit", function () {
-//		createDots();
-//	});
-
-//	createDots();
 
 });
 
